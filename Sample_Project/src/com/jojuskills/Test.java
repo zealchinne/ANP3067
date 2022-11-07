8189f627edadbebfc9087bf37471edecf95f3cac
zealchinne <pedarajuchinnam@gmail.com>
2022-11-07 11:09:23 +0530

    project

---
+++ Sample_Project/src/com/jojuskills/Test.java
@@ -0,0 +1,11 @@
+package com.jojuskills;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p=new Person("Joju",63.50,75,"non-veg");
+		Person p1=new Person("Maha",61.50,55,"veg");
+		System.out.println(p);
+		System.out.print(p1);
+	}
+}
